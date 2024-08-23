0e132ce259e6107574767d5564664d4d98aac6e8
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-08-23 19:23:19 +0530

    Organized the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
+import KrishnaImage from './images/Krishna.webp'
+import RamaImage from './images/Rama.avif'
+import Parashurama from './images/Parashurama.avif'
 
 function App() {
+  const avatars = {
+    a1: { name: 'Matsya' },
+    a2: 'Kurma',
+    a3: 'Varaha',
+    a4: 'Narasimha',
+    a5: 'Vamana',
+    a6: { name: 'Parashurama', title: 'Warrior', idol: Parashurama },
+    a7: { name: 'Sri Rama', title: 'Maryada Purushottama', idol: RamaImage },
+    a8: { name: 'Sri Krishna', title: 'Supreme Consciousness', idol: KrishnaImage },
+    a9: 'Buddha',
+    a10: 'Kalki'
+  };
 
-  let avatar = {
-    a1 : 'Matsya',
-    a2 : 'Kurma',
-    a3 : 'Varaha',
-    a4 : 'Narasimha',
-    a5 : 'Vamana',
-    a6 : 'Parashurama',
-    a7 : 'Sri Rama',
-    a8 : 'Sri Krishna',
-    a9 : 'Buddha',
-    a10 : 'Kalki'
-  }
   return (
     <>
       <h1 className='text-2xl bg-green-700 p-3 rounded-md'>Avatara Purusha</h1>
-      <Card avatar={avatar.a8} state="Super Consciousness"/>
-      <Card avatar={avatar.a7} state="Maryada Purushottama"/>
-      <Card avatar={avatar.a6} state="Warrior"/>
+      <Card avatar={avatars.a8.name} title={avatars.a8.title} idol={avatars.a8.idol} />
+      <Card avatar={avatars.a7.name} title={avatars.a7.title} idol={avatars.a7.idol} />
+      <Card avatar={avatars.a6.name} title={avatars.a6.title} idol={avatars.a6.idol} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import krishnaImage from '../images/Krishna.webp'
+import KrishnaImage from '../images/Krishna.webp'
 
-function Card ({avatar = " AK ", state = "Not assigned yet"}) {
+function Card ({avatar = "Vishnu", title = "Supreme Consciousness", idol = KrishnaImage}) {
   // console.log(props);
   return (
     <div>
       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src={krishnaImage} alt="" width="384" height="512"/>
+        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src={idol} alt="" width="384" height="512"/>
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
             <p className="text-lg font-medium">
@@ -18,7 +18,7 @@ function Card ({avatar = " AK ", state = "Not assigned yet"}) {
               {avatar}
             </div>
             <div className="text-slate-700 dark:text-slate-500">
-              {state}
+              {title}
             </div>
           </figcaption>
         </div>
