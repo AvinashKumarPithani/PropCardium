ed1b8cb853c92e2cb7e0c6d6e7c0a82e5d313e33
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-08-23 18:29:01 +0530

    Handled Card features

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  let avatar = {
+    a1 : 'Matsya',
+    a2 : 'Kurma',
+    a3 : 'Varaha',
+    a4 : 'Narasimha',
+    a5 : 'Vamana',
+    a6 : 'Parashurama',
+    a7 : 'Sri Rama',
+    a8 : 'Sri Krishna',
+    a9 : 'Buddha',
+    a10 : 'Kalki'
+  }
   return (
     <>
-      <h1 className='text-2xl bg-green-700 p-3 rounded-md'>PropCardium</h1>
-      <Card username="Sri Krishna" post="Super Consciousness"/>
-      <Card />
-      <Card />
+      <h1 className='text-2xl bg-green-700 p-3 rounded-md'>Avatara Purusha</h1>
+      <Card avatar={avatar.a8} state="Super Consciousness"/>
+      <Card avatar={avatar.a7} state="Maryada Purushottama"/>
+      <Card avatar={avatar.a6} state="Warrior"/>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,24 @@
 import React from "react";
 import krishnaImage from '../images/Krishna.webp'
 
-function Card ({username = " AK ", post = "Not assigned yet"}) {
+function Card ({avatar = " AK ", state = "Not assigned yet"}) {
   // console.log(props);
   return (
     <div>
-      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <img
-          className="w-24 h-24 rounded-full mx-auto"
-          src={krishnaImage}
-          alt=""
-          width="384"
-          height="512"
-        />
-        <div className="pt-6 space-y-4">
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src={krishnaImage} alt="" width="384" height="512"/>
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
             <p className="text-lg font-medium">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-              eius distinctio, fugiat beatae quae amet dolor aliquid natus sit
-              pariatur?
+              Lorem ipsum dolor sit amet consectetur adipisicing.
             </p>
           </blockquote>
           <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400">{username}</div>
-            <div className="text-slate-700 dark:text-slat">
-              {post}
+            <div className="text-sky-500 dark:text-sky-400">
+              {avatar}
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              {state}
             </div>
           </figcaption>
         </div>
